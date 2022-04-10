96756ae0024903c6debf5b3ce22024387ac60591
Hakan Durmaz <durmazhakan@icloud.com>
2022-04-10 11:38:47 +0300

    Initial commit

---
+++ src/main/java/com/turkcell/elearner/domain/AccountType.java
@@ -0,0 +1,30 @@
+package com.turkcell.elearner.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "account_types")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class AccountType {
+
+    @Id
+    @Column(name = "accountTypeId")
+    private String accountTypeId;
+
+    @Column(name = "accountName")
+    private String accountName;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "description")
+    private String description;
+}
